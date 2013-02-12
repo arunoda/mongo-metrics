dc0ee00941b88159e35a6a567157ad98652f9239
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-02-13 02:03:19 +0530

    added webapp implemented backend JSON api for data transfer implemented simplified Graphing API on top op nvd3

---
+++ webapp.js
@@ -0,0 +1,21 @@
+var express 		= require('express');
+var routes 			= require('./lib/webapp/routes');
+var MongoMetrics 	= require('./lib/mongoMetrics');
+var path			= require('path');
+
+exports.listen = function(mongoUrl, port) {
+
+	var mongoMetrics = new MongoMetrics(mongoUrl);
+	var app = express();
+
+	var currDir = path.dirname(__filename);
+
+	app.engine('ejs', require('ejs').renderFile);
+	app.set('views', path.resolve(currDir, 'lib/webapp/views'));
+	app.use(express.static(path.resolve(currDir, 'lib/webapp/public')));
+	app.use(express.bodyParser());
+
+	routes(mongoMetrics, app);
+
+	app.listen(port);
+};
